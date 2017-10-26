56c89a23b31582b23ad24d129b60a7828de3b6ba
TJ Holowaychuk <tj@vision-media.ca>
2017-10-26 11:33:35 -0700

    http/request: add Error

---
+++ http/request/error.go
@@ -0,0 +1,13 @@
+package request
+
+import "net/http"
+
+// Error is an HTTP error.
+type Error struct {
+	Status int
+}
+
+// Error implementation.
+func (e *Error) Error() string {
+	return http.StatusText(e.Status)
+}
