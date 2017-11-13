86078fbeca77993eb9e5658f6acd94a500a2a3aa
TJ Holowaychuk <tj@vision-media.ca>
2017-11-13 10:41:53 -0800

    http/request: add IsStatus()

--- http/request/error.go
+++ http/request/error.go
@@ -10,8 +10,13 @@ func (e Error) Error() string {
 	return http.StatusText(int(e))
 }
 
+// IsStatus returns true if err is status code.
+func IsStatus(err error, code int) bool {
+	e, ok := err.(Error)
+	return ok && int(e) == code
+}
+
 // IsNotFound returns true if err is a 404.
 func IsNotFound(err error) bool {
-	e, ok := err.(Error)
-	return ok && e == 404
+	return IsStatus(err, 404)
 }
--- http/request/error_test.go
+++ http/request/error_test.go
@@ -9,6 +9,15 @@ import (
 func TestError(t *testing.T) {
 	assert.Equal(t, "Not Found", Error(404).Error())
 	assert.Equal(t, "Bad Request", Error(400).Error())
+}
+
+func TestIsNotFound(t *testing.T) {
 	assert.True(t, IsNotFound(Error(404)))
 	assert.False(t, IsNotFound(Error(500)))
+}
+
+func TestIsStatus(t *testing.T) {
+	assert.True(t, IsStatus(Error(404), 404))
+	assert.True(t, IsStatus(Error(500), 500))
+	assert.False(t, IsStatus(Error(500), 400))
 }
