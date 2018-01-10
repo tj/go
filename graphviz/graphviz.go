73d341dcf5534a560f8ba07407247be6a5532279
TJ Holowaychuk <tj@vision-media.ca>
2018-01-09 18:28:56 -0800

    graphviz: add OpenDot()

---
+++ graphviz/graphviz.go
@@ -0,0 +1,53 @@
+// Package graphviz provides some dot(1) utilities.
+package graphviz
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os/exec"
+
+	"github.com/pkg/browser"
+	"github.com/pkg/errors"
+)
+
+// html template.
+var html = `<html>
+<body>
+  <script src="https://cdn.rawgit.com/anvaka/panzoom/v4.0.0/dist/panzoom.min.js"></script>
+    <div id="graph">{graph}</div>
+    <script>
+      panzoom(document.querySelector('#graph'))
+    </script>
+</body>
+</html>`
+
+// render template.
+func render(graph []byte) []byte {
+	return bytes.Replace([]byte(html), []byte("{graph}"), graph, -1)
+}
+
+// OpenDot opens the given reader as zoomable SVG in the browser.
+func OpenDot(r io.Reader) error {
+	cmd := exec.Command("dot", "-Tsvg")
+	cmd.Stdin = r
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, "executing")
+	}
+
+	err = ioutil.WriteFile("/tmp/graph.html", render(b), 0755)
+	if err != nil {
+		return errors.Wrap(err, "writing")
+	}
+
+	return browser.OpenURL("file:///tmp/out.html")
+}
+
+// Must helper.
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
