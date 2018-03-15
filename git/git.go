0da5c990d77db2de729f3fb2bbf7bad480187f61
TJ Holowaychuk <tj@vision-media.ca>
2018-03-15 09:40:00 -0700

    git: remove wrapping of error
    no point obscuring these really

--- git/git.go
+++ git/git.go
@@ -77,7 +77,7 @@ func (c *Commit) Describe() string {
 func GetCommit(dir, commit string) (c *Commit, err error) {
 	dir, err = GetRoot(dir)
 	if err != nil {
-		return nil, errors.Wrap(err, "getting git repo root")
+		return nil, err
 	}
 
 	cmd := exec.Command("git", "log", "-1", `--pretty=format:{"commit":"%H","abbreviated_commit":"%h","tree":"%T","abbreviated_tree":"%t","parent":"%P","abbreviated_parent":"%p","refs":"%D","encoding":"%e","subject":"%s","sanitized_subject_line":"%f","commit_notes":"%N","verification_flag":"%G?","signer":"%GS","signer_key":"%GK","author":{"name":"%aN","email":"%aE","date":"%aD"},"commiter":{"name":"%cN","email":"%cE","date":"%cD"}}`, commit)
